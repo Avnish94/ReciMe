93133da3a4f94b86a91b223bf681a9d4e09e18ac
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2019-10-28 18:46:29 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+/*script practice file */
+
+function getSearch(){
+	var input = document.getElementById("searchBar").value;
+	console.log(input);
+	if(input=="hello"){
+		document.getElementById("bar").style.backgroundColor= "red";
+	}
+	else{
+		document.getElementById("bar").style.backgroundColor= "cyan";
+	}
+
+	return input;
+}
+
+function search(){
+	var input = document.getElementById("searchBar").value;
+}
+
+function hoverControl(overlay){
+	var overlay =document.getElementById(overlay);
+	overlay.style.opacity=0.75;
+	console.log(overlay.id);
+	var string= overlay.id+"Text";
+
+	var text = document.getElementById(overlay.id+"Text");
+
+
+	if(overlay.id == "overlay1"){
+		overlay.style.backgroundColor="red";
+		text.innerHTML = "UNHEALTHY";
+
+	}
+
+		if(overlay.id == "overlay2"){
+		overlay.style.backgroundColor="green";
+		text.innerHTML = "HEALTHY";
+
+	}
+		if(overlay.id == "overlay3"){
+		overlay.style.backgroundColor="yellow";
+		text.style.color= "black";
+		text.innerHTML = "HIGH IN FATS, BUT LOW ON SUGAR";
+
+	}
+		if(overlay.id == "overlay4"){
+		overlay.style.backgroundColor="orange";
+		text.innerHTML = "HIGH IN sugar, BUT LOW ON starch";
+
+	}
+
+			if(overlay.id == "overlay5"){
+		overlay.style.backgroundColor="green";
+		text.innerHTML = "a heathy dish";
+
+	}
+			if(overlay.id == "overlay6"){
+		overlay.style.backgroundColor="red";
+		text.innerHTML = "HIGH IN sugar AND fat";
+
+	}
+	if(overlay.id == "overlay7"){
+		overlay.style.backgroundColor="purple";
+		text.innerHTML = "FAT ASS";
+
+	}
+		if(overlay.id == "overlay8"){
+		overlay.style.backgroundColor="magenta";
+		text.innerHTML = "exotic meal";
+
+	}
+
+
+
+}
+
+function hoverControlStop(overlay){
+	var overlay =document.getElementById(overlay);
+	overlay.style.opacity=0;
+	overlay.style.backgroundColor="blue";
+}
+
+
+
+
