1ef7c0c293a215ef3b907b5a2c87cf060033ce6a
eakr9014 <ean1001@gmail.com>
2019-12-04 23:41:33 -0700

    updated package.json

--- create_tables.sql
+++ create_tables.sql
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS favorites (
   PRIMARY KEY(user_name, recipe) /* Username can't be the same */
   );
 
-  CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
   user_name VARCHAR(30),
   password VARCHAR(30),
   PRIMARY KEY(user_name) /* Username can't be the same */
